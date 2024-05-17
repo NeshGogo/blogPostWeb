ba82ff77956c04ed74d91a5473d4c08576c1880e
NeshGogo <rafaelaguero10@hotmail.com>
2024-05-17 10:19:43 -0400

    changes: Login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,12 +3,40 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider'
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatDividerModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginFrom = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  login(event:Event){
+    event.preventDefault();
+
+    if(this.loginFrom.invalid)
+      return;
+
+    console.log(this.loginFrom.value);
+  }
+}
