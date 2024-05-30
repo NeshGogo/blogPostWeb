e14ce9ab0acf489ec06c59fe0d3c255327912f89
NeshGogo <rafaelaguero10@hotmail.com>
2024-05-30 17:30:58 -0400

    Changes: Create post dialog

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,11 @@
 import { Component, HostListener, OnInit, signal } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,6 +16,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { Observable, map, of, startWith } from 'rxjs';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-layout',
@@ -38,6 +52,8 @@ export class LayoutComponent implements OnInit {
   filteredOptions: Observable<string[]> = of([]);
   desktopSize = signal<boolean>(window.innerWidth >= 1024);
 
+  constructor(public postCreationDialog: MatDialog) {}
+
   ngOnInit() {
     this.filteredOptions = this.searchField.valueChanges.pipe(
       startWith(''),
@@ -46,12 +62,16 @@ export class LayoutComponent implements OnInit {
   }
 
   @HostListener('window:resize', ['$event'])
-  onResize(event: Event){
-    if(window.innerWidth >= 1024){
+  onResize(event: Event) {
+    if (window.innerWidth >= 1024) {
       this.desktopSize.set(true);
-    }else if(this.desktopSize()){
+    } else if (this.desktopSize()) {
       this.desktopSize.set(false);
     }
+  }
+
+  openPostCreationDialog() {
+    const dialogRef = this.postCreationDialog.open(DialogForPostCreation);
   }
 
   private _filter(value: string): string[] {
@@ -61,4 +81,70 @@ export class LayoutComponent implements OnInit {
       option.toLowerCase().includes(filterValue)
     );
   }
+}
+
+@Component({
+  selector: 'dialog-post-creation',
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatDividerModule,
+    MatDialogContent,
+    MatDialogActions,
+    MatButtonModule,
+  ],
+  template: `
+    <h4 mat-dialog-title>Create new post</h4>
+    <mat-divider></mat-divider>
+    <mat-dialog-content>
+      <form [formGroup]="postFrom">
+        <input
+          type="file"
+          placeholder="Drag a photo here.."
+          aria-label="Select a photo"
+          formControlName="file"
+        />
+
+        <mat-form-field>
+          <mat-label>Description</mat-label>
+          <textarea
+            matInput
+            rows="10"
+            placeholder="Add a description to the post"
+            formControlName="description"
+          ></textarea>
+        </mat-form-field>
+      </form>
+    </mat-dialog-content>
+    <mat-divider></mat-divider>
+    <mat-dialog-actions align="end">
+      <button mat-button mat-dialog-close>Cancel</button>
+      <button mat-button [mat-dialog-close]="true" cdkFocusInitial>
+        Share
+      </button>
+    </mat-dialog-actions>
+  `,
+  styles: `
+    h4 {
+      margin: 0;
+      text-align: center;
+    }
+ 
+    input {
+      margin-bottom: 20px;
+      text-align: center;
+    }
+  `,
+})
+export class DialogForPostCreation {
+  postFrom = new FormGroup({
+    file: new FormControl('', [Validators.required]),
+    description: new FormControl(''),
+  });
+
+  constructor(public dialogRef: MatDialogRef<DialogForPostCreation>) {}
 }
