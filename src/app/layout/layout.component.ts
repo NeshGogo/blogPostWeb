34d152d635939f4db076a95ee635faadb599e5d2
NeshGogo <rafaelaguero10@hotmail.com>
2024-05-30 12:32:11 -0400

    Changes: layout finished

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, signal } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { Observable, map, of, startWith } from 'rxjs';
 
 @Component({
@@ -20,6 +23,11 @@ import { Observable, map, of, startWith } from 'rxjs';
     MatAutocompleteModule,
     AsyncPipe,
     RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    RouterLink,
+    RouterLinkActive,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
@@ -28,12 +36,22 @@ export class LayoutComponent implements OnInit {
   searchField = new FormControl('');
   options: string[] = ['One', 'Two', 'Three'];
   filteredOptions: Observable<string[]> = of([]);
+  desktopSize = signal<boolean>(window.innerWidth >= 1024);
 
   ngOnInit() {
     this.filteredOptions = this.searchField.valueChanges.pipe(
       startWith(''),
       map((value) => this._filter(value || ''))
     );
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event){
+    if(window.innerWidth >= 1024){
+      this.desktopSize.set(true);
+    }else if(this.desktopSize()){
+      this.desktopSize.set(false);
+    }
   }
 
   private _filter(value: string): string[] {
