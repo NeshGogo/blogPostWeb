607481c1a256f468e3a652d35ec947a3ab35a97f
NeshGogo <rafaelaguero10@hotmail.com>
2024-07-01 21:01:25 -0400

    Changes: profile post grid

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Post } from '../models/Post';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule, MatDividerModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  posts =  signal<Post[]>([]);
 
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.postService.getAll(true).subscribe((data) => {
+      this.posts.set(data);
+      this.postService.posts.set(data);
+    });
+  }
 }
