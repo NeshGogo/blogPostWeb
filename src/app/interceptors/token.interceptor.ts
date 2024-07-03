669c42a984cac57afb157fca78c9323ec09653ec
NeshGogo <rafaelaguero10@hotmail.com>
2024-07-02 21:27:51 -0400

    Changes: refresh token when it is expired

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,20 +1,40 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { TokenService } from '../services/token.service';
-import { catchError, throwError } from 'rxjs';
+import { catchError, switchMap, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const tokenService = inject(TokenService);
-  const tokenModel = tokenService.get();
-  if (tokenModel?.token){
+  const router = inject(Router);
+  let tokenModel = tokenService.get();
+
+  if (tokenModel?.token) {
     req = req.clone({
       headers: req.headers.set('authorization', `bearer ${tokenModel?.token}`),
     });
   }
+
   return next(req).pipe(
     catchError((err) => {
-      if (err.status === 401) {
-        tokenService.remove();
+      if (err.status === 401 && tokenModel?.token && tokenService.isTokenExpired()) {
+        return tokenService.refreshToken().pipe(
+          switchMap((newtoken) => {
+            console.log('Expired');
+            req = req.clone({
+              headers: req.headers.set(
+                'authorization',
+                `bearer ${newtoken?.token}`
+              ),
+            });
+            return next(req);
+          }),
+          catchError((err) => {
+            tokenService.remove();
+            router.navigate(['/auth/login']);
+            return throwError(() => err);
+          })
+        )
       }
       return throwError(() => err);
     })
--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from 'jwt-decode';
 import { Token } from '../models/Token';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +11,9 @@ import { Token } from '../models/Token';
 export class TokenService {
   private readonly tokenName = 'token';
   private readonly refreshTokenName = 'refresh_token';
+  private readonly API = `${environment.api}/token`;
+
+  constructor(private http: HttpClient) {}
 
   set(token: string, refreshToken: string) {
     localStorage.setItem(this.tokenName, token);
@@ -23,11 +29,11 @@ export class TokenService {
     return tokenComp?.token ? jwtDecode(tokenComp.token) : null;
   }
 
-  get() : Token {
+  get(): Token {
     return {
       token: localStorage.getItem(this.tokenName),
       refreshToken: localStorage.getItem(this.refreshTokenName),
-    }
+    };
   }
 
   remove() {
@@ -36,5 +42,20 @@ export class TokenService {
 
   removeRefreshToken() {
     localStorage.removeItem(this.refreshTokenName);
+  }
+
+  isTokenExpired() {
+    const expiryTime = this.decodeToken()?.exp;
+    if (expiryTime) {
+      const date = new Date(expiryTime * 1000);
+      return date < new Date();
+    }
+    return true;
+  }
+
+  refreshToken() {
+    return this.http.post<Token>(`${this.API}/refresh`, this.get()).pipe(tap((token) => {
+      this.set(<string>token.token, <string>token.refreshToken);
+    }));
   }
 }
