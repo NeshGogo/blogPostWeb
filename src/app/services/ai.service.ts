f20f52f9b78bced96dec90be603330aeb36e07c0
NeshGogo <rafaelaguero10@hotmail.com>
2024-09-12 19:30:56 -0400

    Changes: AI service

---
+++ src/app/services/ai.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AiService {
+  private readonly API = `${environment.api}/ai`;
+
+  constructor(private http: HttpClient) { }
+
+  GenerateImageCaption(file: File){
+    const body = new FormData();
+    body.set('file', file);
+    return this.http.post<string>(this.API, body);
+  }
+}
